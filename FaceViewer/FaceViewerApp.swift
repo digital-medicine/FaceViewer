6b7bcb813344540343559b938869913cd6ad4419
Florian Schweizer <florian.schweizer@tum.de>
2023-10-25 09:41:13 +0200

    Initial Commit

---
+++ FaceViewer/FaceViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceViewerApp.swift
+//  FaceViewer
+//
+//  Created by Florian Schweizer on 25.10.23.
+//
+
+import SwiftUI
+
+@main
+struct FaceViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
